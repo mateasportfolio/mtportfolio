c7ed9892d899556f81a18bbff19a4ad88ec81260
mateasportfolio <104080590+mateasportfolio@users.noreply.github.com>
2022-12-04 16:26:21 +0100

    AD

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,39 +1,46 @@
 import React from "react";
-import { Text, Stack, Heading } from "@chakra-ui/react";
+import { Stack, Heading } from "@chakra-ui/react";
 import { ListItem, UnorderedList } from "@chakra-ui/react";
+// import { CheckCircleIcon } from "@chakra-ui/icons";
+
 const AboutMe = () => {
   return (
-    <Stack spacing={3} bg="violet" p="40px" m="20px">
-      <Heading p="20px" m="20px" fontSize="6xl">
-        About Me
-      </Heading>
-
-      <Heading size="xl">Education</Heading>
-      <UnorderedList p="20px" m="3px">
-        <ListItem>
-          2022 - Internship Webdesign / Webdevelopment at epekworks
-        </ListItem>
-        <ListItem>
-          2021-Aktuell GPB Gesellschaft für Personalentwicklung und Bildung mbh
-          ( Apprentice as a Media designer - Digital und Print, Schwerpunkt Web)
-        </ListItem>
-        <ListItem>
-          {" "}
-          2008-2012 Language High School Pitagora ,Croatia 2006- 2008 Skalice
-          Primary school, Croatia{" "}
-        </ListItem>
-        <ListItem>2005- 2006 Goethe Comprehensive School, Dieburg</ListItem>
-        <ListItem>
-          {" "}
-          2000-2004 Ysenburg Primary School Hailer-Meerholz, Gelnhausen
-        </ListItem>
-      </UnorderedList>
-      <Heading size="xl">Skills</Heading>
-      <UnorderedList p="20px" m="3px">
-        <ListItem>Html, Css, JavaScript, React.js</ListItem>
-        <Text></Text>
-      </UnorderedList>
-    </Stack>
+    <>
+      <Stack spacing={3} bg="violet" p="30px" m="20px">
+        <Heading fontSize="5xl">About Me</Heading>
+        <Heading as="abbr" size="Xl">
+          Education
+        </Heading>
+        <UnorderedList>
+          <ListItem>
+            2022 - Internship Webdesign / Webdevelopment at epekworks
+          </ListItem>
+          <ListItem>
+            2021-Aktuell GPB Gesellschaft für Personalentwicklung und Bildung
+            mbh ( Apprentice as a Media designer - Digital und Print,
+            Schwerpunkt Web)
+          </ListItem>
+          <ListItem>
+            {" "}
+            2008-2012 Language High School Pitagora ,Croatia 2006- 2008 Skalice
+            Primary school, Croatia{" "}
+          </ListItem>
+          <ListItem>2005- 2006 Goethe Comprehensive School, Dieburg</ListItem>
+          <ListItem>
+            2000-2004 Ysenburg Primary School Hailer-Meerholz, Gelnhausen
+          </ListItem>
+        </UnorderedList>
+        <UnorderedList>
+          <Heading size="Xl">Language</Heading>
+          <ListItem>German, English, Croatian</ListItem>
+        </UnorderedList>
+        <Heading size="Xl">Skills</Heading>
+        <UnorderedList>
+          <ListItem>Html, Css, JavaScript, React.js</ListItem>
+          <ListItem>Illustrator, Photoshop, InDesign, XD</ListItem>
+        </UnorderedList>
+      </Stack>
+    </>
   );
 };
 
