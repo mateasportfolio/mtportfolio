e7db8ccac5f4d3225e115997a89b07d4856a8c2d
mateasportfolio <104080590+mateasportfolio@users.noreply.github.com>
2022-12-04 16:43:59 +0100

    add linear gradient

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -6,36 +6,42 @@ import { ListItem, UnorderedList } from "@chakra-ui/react";
 const AboutMe = () => {
   return (
     <>
-      <Stack spacing={3} bg="violet" p="30px" m="20px">
-        <Heading fontSize="5xl">About Me</Heading>
-        <Heading as="abbr" size="Xl">
+      <Stack bg="linear-gradient(#A88BEB ,#F8CEEC)" p="30px" m="20px">
+        <Heading color="whiteAlpha.700" fontSize="5xl">
+          About Me
+        </Heading>
+        <Heading color="whiteAlpha.700" as="abbr" size="Xl">
           Education
         </Heading>
         <UnorderedList>
-          <ListItem>
+          <ListItem color="whiteAlpha.700">
             2022 - Internship Webdesign / Webdevelopment at epekworks
           </ListItem>
-          <ListItem>
+          <ListItem color="whiteAlpha.700">
             2021-Aktuell GPB Gesellschaft für Personalentwicklung und Bildung
             mbh ( Apprentice as a Media designer - Digital und Print,
             Schwerpunkt Web)
           </ListItem>
-          <ListItem>
+          <ListItem color="whiteAlpha.700">
             {" "}
             2008-2012 Language High School Pitagora ,Croatia 2006- 2008 Skalice
             Primary school, Croatia{" "}
           </ListItem>
-          <ListItem>2005- 2006 Goethe Comprehensive School, Dieburg</ListItem>
-          <ListItem>
+          <ListItem color="whiteAlpha.700">
+            2005- 2006 Goethe Comprehensive School, Dieburg
+          </ListItem>
+          <ListItem color="whiteAlpha.700">
             2000-2004 Ysenburg Primary School Hailer-Meerholz, Gelnhausen
           </ListItem>
         </UnorderedList>
-        <UnorderedList>
+        <UnorderedList color="whiteAlpha.700">
           <Heading size="Xl">Language</Heading>
           <ListItem>German, English, Croatian</ListItem>
         </UnorderedList>
-        <Heading size="Xl">Skills</Heading>
-        <UnorderedList>
+        <Heading color="whiteAlpha.700" size="Xl">
+          Skills
+        </Heading>
+        <UnorderedList color="whiteAlpha.700">
           <ListItem>Html, Css, JavaScript, React.js</ListItem>
           <ListItem>Illustrator, Photoshop, InDesign, XD</ListItem>
         </UnorderedList>
