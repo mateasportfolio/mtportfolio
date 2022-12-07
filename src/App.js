9b52920477043fea6d50f820e817e7fa16e25e47
mateasportfolio <104080590+mateasportfolio@users.noreply.github.com>
2022-12-07 14:06:34 +0100

    changed layout and design to responsive with text component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useColorMode, VStack, useMediaQuery } from "@chakra-ui/react";
+import "./styles.css";
+import { VStack, useMediaQuery, useColorMode } from "@chakra-ui/react"; //
 import { Route, Routes } from "react-router-dom";
 import { Header } from "./components/Header";
 import Home from "./pages/Home";
@@ -8,21 +9,24 @@ import Art from "./pages/Art";
 import AboutMe from "./pages/AboutMe";
 
 function App() {
+  // const App = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   const isDark = colorMode === "dark";
   const [isNotSmallerScreen] = useMediaQuery("(min-width: 768px)");
 
   return (
-    <VStack p={5} mt={isNotSmallerScreen ? "0" : 16} align="flex-start">
-      <Header isDark={isDark} toggleColorMode={toggleColorMode} />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/aboutme" element={<AboutMe />} />
-        <Route path="/art" element={<Art />} />
-        <Route path="/code" element={<Code />} />
-        <Route path="/hosting" element={<Hosting />} />
-      </Routes>
-    </VStack>
+    <>
+      <VStack p={5} mt={isNotSmallerScreen ? "0" : 16} align="flex-start">
+        <Header isDark={isDark} toggleColorMode={toggleColorMode} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/aboutme" element={<AboutMe />} />
+          <Route path="/art" element={<Art />} />
+          <Route path="/code" element={<Code />} />
+          <Route path="/hosting" element={<Hosting />} />
+        </Routes>
+      </VStack>
+    </>
   );
 }
 export default App;
--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,126 +1,125 @@
 import React from "react";
-import { Box, useMediaQuery, VStack, Icon } from "@chakra-ui/react";
-import { Input } from "@chakra-ui/react";
-import { Stack, Heading } from "@chakra-ui/react";
-import { ListItem, UnorderedList } from "@chakra-ui/react";
-
-import { FaReact, FaHtml5 } from "react-icons/fa";
+import {
+  Text,
+  Stack,
+  Flex,
+  useMediaQuery,
+  Input,
+  Box,
+  Heading,
+} from "@chakra-ui/react";
 
 const AboutMe = () => {
   const [isNotSmallerScreen] = useMediaQuery("(min-width: 768px)");
   return (
-    <Stack with="100%" direction="column" alignSelf="center">
-      <Box mt={isNotSmallerScreen ? "0" : 16} alignSelf="center">
-        {" "}
-      </Box>
-      <VStack bg="linear-gradient(#A88BEB ,#F8CEEC)" p="30px" m="3rem">
-        <Heading color="whiteAlpha.700" size="xl">
-          Education
-        </Heading>
-        <UnorderedList>
-          <ListItem>
-            2022 - Internship Webdesign / Webdevelopment / Marketing at
-            epekworks
-          </ListItem>
-          <ListItem>
-            2021-Aktuell GPB Gesellschaft für Personalentwicklung und Bildung
-            mbh ( Apprentice as a Media designer - Digital und Print,
-            Schwerpunkt Web)
-          </ListItem>
-          <ListItem>
-            {" "}
-            2008-2012 Language High School Pitagora ,Croatia 2006- 2008 Skalice
-            Primary school, Croatia{" "}
-          </ListItem>
-          <ListItem>2005- 2006 Goethe Comprehensive School, Dieburg</ListItem>
-          <ListItem>
-            2000-2004 Ysenburg Primary School Hailer-Meerholz, Gelnhausen
-          </ListItem>
-          <Heading color="whiteAlpha.900" size="lg">
-            Skills
-          </Heading>
-          <ListItem>
-            <Icon
-              as={FaReact}
-              w={8}
-              h={8}
-              color="blue.500"
-              direction="row"
-              display="flow"
-            />
-            <Icon
-              as={FaHtml5}
-              w={8}
-              h={8}
-              color="blue.500"
-              direction="column"
-              display="flow"
-            />
-            Html, Css, JavaScript,React.js{" "}
-          </ListItem>
-
-          <ListItem>Illustrator, Photoshop, InDesign, XD</ListItem>
-          <Heading
-            color="whiteAlpha.900"
-            size="lg"
-            fontFamily="sans-serif"
-            fontWeight="light"
-          >
-            Language
-          </Heading>
-          <ListItem>German, Croatian, English</ListItem>
-        </UnorderedList>
-      </VStack>
-
-      <Box
-        display="flex"
-        border="2px solid red"
-        justifyContent="center"
-        pb="20px"
-        color="blueviolet"
+    <Flex
+      with="100%"
+      direction="column"
+      alignSelf="center"
+      p="20px"
+      m="10px"
+      justifyContent="center"
+      justifySelf="baseline"
+    >
+      <Flex
+        mt={isNotSmallerScreen ? "0" : 16}
+        alignSelf="center"
+        justifySelf="center"
       >
-        <Stack
-          spacing={6}
-          color="blueviolet"
-          borderRadius="3px"
-          borderColor="lightpink"
-        >
-          <Heading size="xl" pt="30px" alignSelf="center">
-            Contact
-          </Heading>
-          <Input
-            borderRadius="3px"
-            color="blueviolet"
-            placeholder="017659017116"
-            size="sm"
-            type="number"
-            fontWeight={600}
-            as="input"
-          />
-          <Input
-            borderRadius="3px"
-            placeholder="mateatabak@gmail.com"
-            size="sm"
-            type="email"
-            fontWeight={600}
-            as="input"
-            color="blueviolet"
-          />
+        {" "}
+      </Flex>
 
-          <Input
-            borderRadius="3px"
-            placeholder="Schloßstr.128,12163 "
-            size="sm"
-            type="text"
-            fontWeight={600}
-            as="input"
+      <Stack spacing={3}>
+        <Text noOfLines={[1, 2, 3]} fontSize="4xl">
+          {" "}
+          Education
+        </Text>
+        <Text noOfLines={[1, 2, 3]} fontSize="2xl">
+          2022 - Internship Webdesign / Webdevelopment / Marketing at epekworks
+        </Text>
+        <Text noOfLines={[1, 2, 3]} fontSize="2xl">
+          2021-Aktuell GPB Gesellschaft für Personalentwicklung und Bildung mbh
+          ( Apprentice as a Media designer - Digital und Print, Schwerpunkt Web)
+        </Text>
+        <Text noOfLines={[1, 2, 3]} fontSize="2xl">
+          2008-2012 Language High School Pitagora ,Croatia 2006- 2008 Skalice
+          Primary school, Croatia
+        </Text>
+        <Text noOfLines={[1, 2, 3]} fontSize="2xl">
+          2005-2006 Goethe Comprehensive School, Dieburg
+        </Text>
+        <Text noOfLines={[1, 2, 3]} fontSize="2xl">
+          2000-2004 Ysenburg Primary School Hailer-Meerholz, Gelnhausen
+        </Text>
+        <br />
+
+        <Text noOfLines={[1, 2, 3]} fontSize="4xl">
+          Skills
+        </Text>
+        <Text noOfLines={[1, 2, 3]} fontSize="1xl">
+          Html, Css, JavaScript,React.js
+        </Text>
+        <Text noOfLines={[1, 2, 3]} fontSize="1xl">
+          Illustrator, Photoshop, InDesign, XD
+        </Text>
+        <br />
+        <Text noOfLines={[1, 2, 3]} fontSize="4xl">
+          Language
+        </Text>
+        <Text noOfLines={[1, 2, 3]} fontSize="2xl">
+          German, Croatian, English and trying to learn Italian
+        </Text>
+        <br />
+        <Box
+          display="flex"
+          border="2px solid red"
+          justifyContent="center"
+          pb="20px"
+          color="blueviolet"
+        >
+          <Stack
+            spacing={6}
             color="blueviolet"
+            borderRadius="3px"
             borderColor="lightpink"
-          />
-        </Stack>
-      </Box>
-    </Stack>
+          >
+            <br />
+            <Heading size="xl" pt="30px" alignSelf="center">
+              Contact
+            </Heading>
+            <Input
+              borderRadius="3px"
+              color="blueviolet"
+              placeholder="017659017116"
+              size="sm"
+              type="number"
+              fontWeight={600}
+              as="input"
+            />
+            <Input
+              borderRadius="3px"
+              placeholder="mateatabak@gmail.com"
+              size="sm"
+              type="email"
+              fontWeight={600}
+              as="input"
+              color="blueviolet"
+            />
+
+            <Input
+              borderRadius="3px"
+              placeholder="Schloßstr.128,12163 "
+              size="sm"
+              type="text"
+              fontWeight={600}
+              as="input"
+              color="blueviolet"
+              borderColor="lightpink"
+            />
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
   );
 };
-
 export default AboutMe;
