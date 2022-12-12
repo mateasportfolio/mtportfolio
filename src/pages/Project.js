8f87b0ff8c8e6cf3baa38fd14b516ed99b0b3dcb
mateasportfolio <104080590+mateasportfolio@users.noreply.github.com>
2022-12-12 21:06:32 +0100

    modifite Project.js

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,49 +1,71 @@
-import { Box, Text, Image, VStack } from "@chakra-ui/react";
+import { Box, Text, Stack, Button, useMediaQuery } from "@chakra-ui/react";
 import React from "react";
-
 import { Link } from "@chakra-ui/react";
 
-import Savings from "../assets/calculator.svg";
-
 export default function Project() {
+  const [isNotSmallerScreen] = useMediaQuery("(min-width: 600px)");
   return (
     <Box>
       <Text as="b" fontSize="4xl" fontWeight="hairline" color="pink.400">
         {" "}
         Made with React.js
       </Text>
-      <VStack direction="row">
-        <Box p="20px" m="50px">
-          <Image
-            dropShadow="inner"
-            boxSize="xl"
-            objectFit="cover"
-            backgroundColor="pink.200"
-            bg="linear-gradient(#ffc0eb, #9198e5)"
-            opacity={10}
-            boxShadow="red"
-            width="500px"
-            h="100%"
-            m="12px"
-            pl="2px"
-            pr="2px"
-            pt="2px"
+      <Stack
+        display="flex"
+        p="2px"
+        position="relative"
+        direction={isNotSmallerScreen ? "row" : "column"}
+        width="50%"
+      >
+        <Box
+          direction={isNotSmallerScreen ? "row" : "column"}
+          width="100%"
+          display="flex"
+          p="2px"
+          m="100px"
+          border="1px solid  #9198e5"
+          justifyContent="center"
+        >
+          <Button
+            border="1px solid  #9198e5"
             position="relative"
-            shadow="outline"
-            src={Savings}
-          />
-          <Link
-            fontSize="2xl"
-            fontWeight="hairline"
-            color="pink.400"
-            href="https://endearing-blini-1b0650.netlify.app"
-            m="30px"
-            p="40px"
+            p="20px"
+            m="10px"
+            display="flex"
           >
-            Savings calculator
-          </Link>
+            <Link
+              position="relative"
+              fontSize="xl"
+              fontWeight="hairline"
+              color="pink.400"
+              href="https://endearing-blini-1b0650.netlify.app"
+              m="30px"
+              p="4px"
+            >
+              Savings calculator
+            </Link>
+          </Button>
+
+          <Button
+            border="1px solid  #9198e5"
+            position="relative"
+            p="20px"
+            m="10px"
+            display="flex"
+          >
+            <Link
+              fontSize="xl"
+              fontWeight="hairline"
+              color="pink.400"
+              href="https://melodious-arithmetic-98d708.netlify.app/"
+              m="30px"
+              p="4px"
+            >
+              Meditation Timer
+            </Link>
+          </Button>
         </Box>
-      </VStack>
+      </Stack>
     </Box>
   );
 }
