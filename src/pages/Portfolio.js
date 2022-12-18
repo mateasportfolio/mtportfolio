4b742519b2eeb57daaee269134af97915e920935
mateasportfolio <104080590+mateasportfolio@users.noreply.github.com>
2022-12-18 13:58:34 +0100

    add lightbox to porfolio.js

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,70 +1,99 @@
-import { Box, Text, Stack, Button, useMediaQuery } from "@chakra-ui/react";
+import {
+  Box,
+  Text,
+  Stack,
+  Button,
+  useMediaQuery,
+  Flex,
+} from "@chakra-ui/react";
 import React from "react";
 import { Link } from "@chakra-ui/react";
+import Lightbox from "yet-another-react-lightbox";
+import { useState } from "react";
+import Faltschachtel from "../assets/portfolio/Faltschachtel.png";
+import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+import "yet-another-react-lightbox/styles.css";
 
 export default function Portfolio() {
   const [isNotSmallerScreen] = useMediaQuery("(min-width: 600px)");
+
+  const [open, setOpen] = useState(false);
+
   return (
-    <Box>
-      <Text as="b" fontSize="4xl" fontWeight="hairline" color="pink.400">
-        {" "}
-        Made with React.js
-      </Text>
-      <Stack
-        display="flex"
-        p="2px"
-        position="relative"
-        direction={isNotSmallerScreen ? "row" : "column"}
-        width="50%"
-      >
-        <Box
-          direction={isNotSmallerScreen ? "row" : "column"}
-          width="100%"
+    <>
+      <Box>
+        <Text as="b" fontSize="4xl" fontWeight="hairline" color="pink.400">
+          {" "}
+          Made with React.js
+        </Text>
+        <Stack
           display="flex"
           p="2px"
-          m="100px"
-          justifyContent="center"
+          position="relative"
+          direction={isNotSmallerScreen ? "row" : "column"}
+          width="50%"
         >
-          <Button
-            border="1px solid  #9198e5"
-            position="relative"
-            p="20px"
-            m="10px"
+          <Box
+            direction={isNotSmallerScreen ? "row" : "column"}
+            width="100%"
             display="flex"
+            p="2px"
+            m="100px"
+            justifyContent="center"
           >
-            <Link
+            <Button
+              border="1px solid  #9198e5"
               position="relative"
-              fontSize="xl"
-              fontWeight="hairline"
-              color="pink.400"
-              href="https://endearing-blini-1b0650.netlify.app"
-              m="30px"
-              p="4px"
+              p="20px"
+              m="10px"
+              display="flex"
             >
-              Savings calculator
-            </Link>
-          </Button>
+              <Link
+                position="relative"
+                fontSize="xl"
+                fontWeight="hairline"
+                color="pink.400"
+                href="https://endearing-blini-1b0650.netlify.app"
+                m="30px"
+                p="4px"
+              >
+                Savings calculator
+              </Link>
+            </Button>
 
-          <Button
-            border="1px solid  #9198e5"
-            position="relative"
-            p="20px"
-            m="10px"
-            display="flex"
-          >
-            <Link
-              fontSize="xl"
-              fontWeight="hairline"
-              color="pink.400"
-              href="https://melodious-arithmetic-98d708.netlify.app/"
-              m="30px"
-              p="4px"
+            <Button
+              border="1px solid  #9198e5"
+              position="relative"
+              p="20px"
+              m="10px"
+              display="flex"
             >
-              Meditation Timer
-            </Link>
-          </Button>
-        </Box>
-      </Stack>
-    </Box>
+              <Link
+                fontSize="xl"
+                fontWeight="hairline"
+                color="pink.400"
+                href="https://melodious-arithmetic-98d708.netlify.app/"
+                m="30px"
+                p="4px"
+              >
+                Meditation Timer
+              </Link>
+            </Button>
+          </Box>
+        </Stack>
+      </Box>
+      <Flex>
+        <button type="button" onClick={() => setOpen(true)}>
+          Open Lightbox
+        </button>
+
+        <Lightbox
+          open={open}
+          close={() => setOpen(false)}
+          slides={[{ src: Faltschachtel, width: 400, height: 400 }]}
+          plugins={[Fullscreen]}
+        />
+      </Flex>
+    </>
   );
 }
