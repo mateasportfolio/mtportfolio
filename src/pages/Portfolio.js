7d720f6761244843981be3a362b7a4d4664fdeb1
mateasportfolio <104080590+mateasportfolio@users.noreply.github.com>
2023-02-08 06:18:16 +0100

    deleted old text component

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -122,7 +122,7 @@ export default function Portfolio() {
                   justifyContent="center"
                   textAlign="center"
                 >
-                  UXUI Design Landingpage
+                  UXUI Design Prototype
                 </Text>
                 <Image
                   src={mohn2}
